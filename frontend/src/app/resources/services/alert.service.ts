e2227585fac38539d7a0d158c4f2b945c21483bd
Yuri Freire <yurifreireg@gmail.com>
2022-01-16 18:15:08 -0300

    Create alert.service

---
+++ frontend/src/app/resources/services/alert.service.ts
@@ -0,0 +1,26 @@
+import { Injectable } from '@angular/core';
+import Swal, { SweetAlertIcon } from 'sweetalert2';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AlertService {
+
+  constructor() { }
+
+  public success(msg: string, title?:any):void {
+    this.showAlert(title, msg, 'success')
+  }
+
+  public info(msg: string, title?:any):void {
+    this.showAlert(title, msg, 'info')
+  }
+
+  public error(msg: string, title?:any):void {
+    this.showAlert(title, msg, 'error')
+  }
+
+  private showAlert(title: string, msg: string, icon: SweetAlertIcon):void {
+    Swal.fire(title, msg, icon)
+  }
+}
